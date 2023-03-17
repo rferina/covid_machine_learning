5d9890f14d434edf3068533adc6239504fc571c6
tonibrook43 <93627688+tonibrook43@users.noreply.github.com>
2023-03-17 12:45:36 -0700

    Update txt_to_csv.py

--- data_cleaning_scripts/IMGT/txt_to_csv.py
+++ data_cleaning_scripts/IMGT/txt_to_csv.py
@@ -7,11 +7,10 @@ import argparse
 import re
 from Bio.Seq import Seq
 from Bio import SeqIO
-#from Bio.SeqIO import csv_instaIO
 from Bio.SeqRecord import SeqRecord
 
 def get_args():
-    parser = argparse.ArgumentParser(description="A program to convert cDNA sequences into protein sequences by searching for codons and translating into amino acids")
+    parser = argparse.ArgumentParser(description="A program to convert .csv file into a one line .csv file using a defined one line function")
     parser.add_argument("-f", "--file", help="designates absolute file path to csv_insta file", required=True)
     parser.add_argument("-o", "--outfile", help="designates absolute file path to the output file", required=True)
     return parser.parse_args()
@@ -25,7 +24,6 @@ def oneline_csv_insta():
     to confirm the output file is accurate.'''
     # make dict with headers as keys and sequences as values
     seq_dict = {}
-    #with open(file, 'r') as fa:
     with open(args.file, 'r') as csv_in, open(args.outfile, 'w') as csv_out:
         line_count = 0
         for line in csv_in:
